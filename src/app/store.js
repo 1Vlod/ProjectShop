5cdc0697b08972cd5d2480265b4f4f42e6bc2313
Vlod <thesuperflintz@gmail.com>
2020-12-07 15:28:08 +0300

    Add test async logic with thunks

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
+
 import counterReducer from '../features/counter/counterSlice'
 import itemsReducer from '../features/items/itemsSlice'
 import userCartReducer from '../features/userCart/userCartSlice'
+import postsReducer from '../features/posts/postsSlice'
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
     items: itemsReducer,
-    userCart: userCartReducer
+    userCart: userCartReducer,
+    posts: postsReducer
   },
 });
--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
@@ -6,22 +6,46 @@ import Col from "react-bootstrap/Col"
 
 import ProductCard from "../components/ProductsPage/ProductCard"
 
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { selectItems } from '../features/items/itemsSlice'
+import { postsSelector, fetchPosts, fetchPostById } from "../features/posts/postsSlice"
+import Button from 'react-bootstrap/esm/Button'
 
 const ProductsPage = () => {
+  const [state, setState] = useState(1)
+  const dispatch = useDispatch()
+  const { loading, posts } = useSelector(postsSelector)
+
   const items = useSelector(selectItems)
 
+  useEffect(() => {
+    dispatch(fetchPosts())
+  }, [dispatch])
 
+  const renderPosts = () => {
+    if (posts.length === 0) return <p>Loading posts...</p>
+
+    return posts.map(item => {
+      return (
+        <Col key={item.id}>
+          {JSON.stringify(item)}
+        </Col>
+      )
+    })
+  }
   return (
     <Container className="mt-5">
-      <h1 className="display-1">Products</h1>
+      <h1 className="display-1">Products {state}</h1>
+      <button onClick={() => setState(state + 1)}>Click me</button>
       <Row>
         {items.map(item => (
           <Col key={item.id}>
             <ProductCard product={item}/>
           </Col>
         ))}
+      </Row>
+      <Row>
+        {renderPosts()}
       </Row>
     </Container>
   )

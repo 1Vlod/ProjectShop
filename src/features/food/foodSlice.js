a644d452b42b2af64f4e7f221c99f9687ec34262
Vlod <thesuperflintz@gmail.com>
2020-12-08 15:09:04 +0300

    complete food page

--- src/features/food/foodSlice.js
+++ src/features/food/foodSlice.js
@@ -1,19 +1,10 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
-
-export const fetchFoodItemById = createAsyncThunk(
-  "posts/fetchByIdStatus",
-  async (postId, thunkAPI) => {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
-    const data = await response.json()
-    return data
-  }
-)
+import { createSlice } from "@reduxjs/toolkit"
 
 export const foodSlice = createSlice({
   name: "Food",
   initialState: {
     loading: "idle",
-    foodItems: []
+    foodItem: {}
   },
   reducers: {
     foodItemLoading(state, action) {
@@ -24,26 +15,27 @@ export const foodSlice = createSlice({
     foodItemReceived(state, action) {
       if (state.loading === "pending") {
         state.loading = "idle"
-        state.foodItems = action.payload
+        state.foodItem = action.payload
       }
     }
-  },
-  extraReducers: {
-    [fetchFoodItemById.fulfilled]: (state, action) => {
-      state.foodItems.push(action.payload)
-    }
   }
-}) 
+})
 
 export const { foodItemLoading, foodItemReceived } = foodSlice.actions
 
-export const fetchFoodItem = () => async dispatch => {
+export const fetchFoodItem = ({ query }) => async dispatch => {
   dispatch(foodItemLoading())
+  let response
 
-  const response = await fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+  if (query) {
+    response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`)
+  } else {
+    response = await fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+  }
+
   const data = await response.json()
-
-  dispatch(foodItemReceived(data.meals))
+  console.log(data)
+  dispatch(foodItemReceived(data.meals[0]))
 }
 
 export const foodSelector = state => state.food
--- src/pages/FoodPage.js
+++ src/pages/FoodPage.js
@@ -4,6 +4,8 @@ import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import Button from 'react-bootstrap/Button'
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
 
 import ProductCard from "../components/ProductsPage/ProductCard"
 
@@ -12,12 +14,14 @@ import { foodSelector, fetchFoodItem } from "../features/food/foodSlice"
 
 const FoodPage = () => {
   const [show, setShow] = useState(false)
+  const [query, setQuery] = useState("")
+
   const dispatch = useDispatch()
-  const { loading, foodItems } = useSelector(foodSelector)
+  const { loading, foodItem } = useSelector(foodSelector)
 
   const getFoodItem = () => {
     setShow(true)
-    dispatch(fetchFoodItem())
+    dispatch(fetchFoodItem({ query }))
   }
 
 
@@ -28,19 +32,55 @@ const FoodPage = () => {
       <Col>
         <ProductCard
           product={{
-            name: foodItems[0].strMeal,
-            img: foodItems[0].strMealThumb,
-            price: 0
+            name: foodItem.strMeal,
+            img: foodItem.strMealThumb,
+            price: foodItem.idMeal.slice(-2),
+            id: foodItem.idMeal
           }}
         />
       </Col>
     )
   }
 
+  const renderBtn = () => {
+    if (query.length > 0) {
+      return (
+        <Button
+          variant="success"
+          onClick={getFoodItem}
+          disabled={loading === "pending"}>
+          Get food!
+        </Button>
+      )
+    }
+
+    return (
+      <Button
+        variant="primary"
+        onClick={getFoodItem}
+        disabled={loading === "pending"}>
+        Get random food!
+      </Button>
+    )
+  }
+
   return (
     <Container className="mt-5">
       <h1 className="display-1">Food Shop</h1>
-      <Button variant="primary" onClick={getFoodItem}>Get random food!</Button>
+      <Row>
+        <Col>
+          <InputGroup className="mb-3">
+            <FormControl
+              value={query}
+              onChange={e => setQuery(e.target.value)}
+              placeholder="Food"
+            />
+          </InputGroup>
+        </Col>
+
+        <Col>{renderBtn()}</Col>
+      </Row>
+
       {show && (
         <Row>
           {renderFoodItem()}
